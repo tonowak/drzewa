af9dbff70e6687ebe03299118e8e6241a1c93441
Mikolaj Wasiak <mikolaj.wasiak@proton.me>
2023-12-30 10:28:10 +0100

    image served as file

--- query/urls.py
+++ query/urls.py
@@ -3,6 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path("map", views.pipe_map, name="map"),
     path("", views.index, name="index"),
     path("<str:loc_x>/<str:loc_y>/", views.location, name="location"),
 ]
--- query/views.py
+++ query/views.py
@@ -1,22 +1,32 @@
 # from django.shortcuts import render
-from django.http import HttpResponse, JsonResponse
-from django.shortcuts import render
+from django.http import HttpResponse, JsonResponse, FileResponse
 from django.views.generic import TemplateView
 from . import logic
 
+picture_name = b"picture.jpg"
+
 
 # Create your views here.
-def index(request):
+def index(_):
     return HttpResponse(b"Hello, world!")
 
 
-def location(request, loc_x, loc_y):
+def location(_, loc_x, loc_y):
     floc_x = float(loc_x)
     floc_y = float(loc_y)
     bolded_map, description = logic.query(floc_x, floc_y)
     print(description)
 
+    map_rgb = bolded_map.convert('RGB')
+    map_rgb.save(picture_name, "JPEG")
+
     return JsonResponse({"description": description})
+
+
+def pipe_map(_):
+    map = open(picture_name, 'rb')
+    response = FileResponse(map)
+    return response
 
 
 class HomePageView(TemplateView):
